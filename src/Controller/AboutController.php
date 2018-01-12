d79a9647386ee6652c8925747e1b09f18d7f8a19
Mikhail Syniuk <msyniuk@gmail.com>
2018-01-12 22:24:51 +0200

    Feedback

--- src/Controller/AboutController.php
+++ src/Controller/AboutController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 
+use App\Entity\Feedback;
 use App\Form\FeedBackType;
+use Doctrine\ORM\EntityManagerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -42,9 +44,34 @@ class AboutController extends Controller
      *
      * @param Request $request
      */
-    public function feedback(Request $request)
+    public function feedback(Request $request, EntityManagerInterface $em, \Swift_Mailer $mailer)
     {
-        $form = $this->createForm(FeedBackType::class);
+        $feedback = new Feedback();
+        $form = $this->createForm(FeedBackType::class, $feedback);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()){
+            //$feedback = $form->getData(); если форма не связана с сущностью
+            $em->persist($feedback);
+            $em->flush();
+
+            $this->addFlash('info', 'Спасибо за обращение!');
+
+            $message = (new \Swift_Message('Обращение с сайта'))
+                ->setFrom([getenv('MAILER_FROM') => getenv('MAILER_FROM_NAME')])
+                ->setTo(getenv('ADMIN_EMAIL'))
+                ->setBody(
+                    $this->renderView(
+                    // templates/emails/registration.html.twig
+                        'about/message.html.twig',
+                        array('feedback' => $feedback)
+                    ),
+                    'text/html'
+                );
+            $mailer->send($message);
+
+            return $this->redirectToRoute('feedback');
+        }
 
         return $this->render('about/feedback.html.twig', [
             'form' => $form->createView(),
